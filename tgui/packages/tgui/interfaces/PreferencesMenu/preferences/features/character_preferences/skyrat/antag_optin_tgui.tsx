5cbab88cc7ffa14d7a5d1b2ea06ccf9f28a3b51f
Lila S <sheepwiththemask@gmail.com>
2023-11-06 16:25:13 +0100

    moves `antag_optin_tgui.tsx` to the proper location

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/skyrat/antag_optin_tgui.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/skyrat/antag_optin_tgui.tsx
@@ -1,8 +1,6 @@
 import { FeatureChoiced, FeatureDropdownInput } from '../../base';
 
 export const antag_opt_in_status_pref: FeatureChoiced = {
-	name: 'Be Antagonist Target',
-	component: FeatureDropdownInput,
+  name: 'Be Antagonist Target',
+  component: FeatureDropdownInput,
 };
-
-
